3f561854fa9db38d5744e77cc223c11bc96ceec3
Hector <hjac10@hotmail.com>
2024-03-21 23:03:36 -0400

    Agregando iconos a las opciones del sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,16 +1,17 @@
 import { Nav } from 'react-bootstrap';
+import { FaAngleDown } from "react-icons/fa6";
 import './styles/sidebar.css';
 
 export const Sidebar = () => {
     return (
         <div className="sidebar">
             <Nav className="flex-column">
-                <Nav.Link className='sidebar-button'>Empresa</Nav.Link>
-                <Nav.Link className='sidebar-button'>Departamentos</Nav.Link>
-                <Nav.Link className='sidebar-button'>Empleados</Nav.Link>
-                <Nav.Link className='sidebar-button'>Proyectos</Nav.Link>
-                <Nav.Link className='sidebar-button'>Planes</Nav.Link>
-                <Nav.Link className='sidebar-button'>Gráficas</Nav.Link>
+                <Nav.Link className='sidebar-button'>Empresa <FaAngleDown /></Nav.Link>
+                <Nav.Link className='sidebar-button'>Departamentos <FaAngleDown /></Nav.Link>
+                <Nav.Link className='sidebar-button'>Empleados <FaAngleDown /></Nav.Link>
+                <Nav.Link className='sidebar-button'>Proyectos <FaAngleDown /></Nav.Link>
+                <Nav.Link className='sidebar-button'>Planes <FaAngleDown /></Nav.Link>
+                <Nav.Link className='sidebar-button'>Gráficas <FaAngleDown /></Nav.Link>
             </Nav>
         </div>
     );
